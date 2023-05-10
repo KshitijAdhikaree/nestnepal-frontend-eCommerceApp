9ecdb3353d4ecb48b179ad2a20c743827d3e030b
Kshitij adhikaree <adhikareekhsitij@gmail.com>
2023-05-10 21:37:41 +0545

    filter electronics

--- src/contexts/ProductContext.js
+++ src/contexts/ProductContext.js
@@ -11,12 +11,12 @@ useEffect(() => {
   const fetchProducts = async () => {
     const response = await fetch('https://fakestoreapi.com/products')
     const data = await response.json()
-    console.log(data)
+    setProducts(data)
   }
   fetchProducts()
 }, [])
 
-return <ProductContext.Provider>{children}</ProductContext.Provider>
+return <ProductContext.Provider value={{products}}>{children}</ProductContext.Provider>
 }
 
 
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useContext } from 'react'
+import { ProductContext } from '../contexts/ProductContext'
 
 const Home = () => {
-  return <div>Homepage</div>;
-};
+  //get all the product from product context
+  const { products } = useContext(ProductContext)
+  console.log(products)
 
-export default Home;
+  //get only electronics category
+  const filteredProducts = products.filter((item) => {
+    return (
+      item.category ==='electronics'
+    )
+  })
+  console.log(filteredProducts);
+  return (
+    <div>
+      <section>
+        <div className='container mx-auto'>
+          <div>{filteredProducts.map((product) => {
+            return <div>
+              {product.title}
+            </div>
+
+          })}</div>
+        </div>
+      </section>
+    </div>
+  )
+
+ 
+}
+
+export default Home
