733abaec2bfdfbe4e809b1f387c3154d9aa0f71d
Kshitij adhikaree <adhikareekhsitij@gmail.com>
2023-05-10 20:49:13 +0545

    app layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
 import React from 'react';
-import {} from 'react-router-dom'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+
+//Import Pages
+import Home from './pages/Home'
+import ProductDetails from './pages/ProductDetails'
+
+//Import Components
+import Header from './components/Header'
+import SideBar from './components/Sidebar'
+import Footer from './components/Footer'
 
 const App = () => {
-  return <div className='overflow-hidden'>react app</div>;
+  return <div className='overflow-hidden'>
+    <Router>
+      <Header/>
+      <Routes>
+        <Route path='/' element={<Home />}></Route>
+        <Route path='/product/:id' element={<ProductDetails />}></Route>
+        <Route path='/' element={<Home />}></Route>
+
+      </Routes>
+      <SideBar/>
+      <Footer/>
+    </Router>
+  </div>;
 };
 
 export default App;
