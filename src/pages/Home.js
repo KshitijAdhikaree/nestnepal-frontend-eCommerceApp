6359ad34ed12cb68e6e0466b6df8fd4c07b34b66
Kshitij adhikaree <adhikareekhsitij@gmail.com>
2023-05-10 22:35:16 +0545

    get product by id

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,34 +1,29 @@
 import React, { useContext } from 'react'
 import { ProductContext } from '../contexts/ProductContext'
+import Product from '../components/Product'
 
 const Home = () => {
   //get all the product from product context
   const { products } = useContext(ProductContext)
-  console.log(products)
 
   //get only electronics category
   const filteredProducts = products.filter((item) => {
-    return (
-      item.category ==='electronics'
-    )
+    return item.category === 'electronics'
   })
-  console.log(filteredProducts);
+  console.log(filteredProducts)
   return (
     <div>
-      <section>
+      <section className='py-18'>
         <div className='container mx-auto'>
-          <div>{filteredProducts.map((product) => {
-            return <div>
-              {product.title}
-            </div>
-
-          })}</div>
+          <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 xl:grid-cols-5 gap-[20px]'>
+            {filteredProducts.map((product) => {
+              return <Product product={product} key={product.id} />
+            })}
+          </div>
         </div>
       </section>
     </div>
   )
-
- 
 }
 
 export default Home
